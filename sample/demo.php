b0fae989e478d07d7641b89936ed30a8cd9465db
locdd <locddspkt@gmail.com>
2020-04-09 13:58:13 +0700

    more example

--- sample/demo.php
+++ sample/demo.php
@@ -9,5 +9,39 @@ $response = SimpleCurl::Get('https://example.com');
 file_put_contents(__DIR__ . '/get.html', $response);
 
 //simple post
-$response = SimpleCurl::Post('https://postman-echo.com/post', array('data1' => 'this is the data item'));
-echo ($response);
+$params = array(
+    'string' => 'text param can be passed',
+    'int' => 100,
+    'float' => 100.59,
+    'date' => '2020-04-09',
+    'time' => '2020-04-19T12:00:00'
+);
+$response = SimpleCurl::Post('https://postman-echo.com/post', $params);
+
+//complex post
+$params = array(
+    'string' => 'text param can be passed',
+    'int' => 100,
+    'float' => 100.59,
+    'date' => '2020-04-09',
+    'time' => '2020-04-19T12:00:00'
+);
+$cookieFilePath = __DIR__ . '/cookie.txt';
+
+$externalHeaders = array(
+    'x-api-key' => 'API key of the request ...',
+    'x-password' => 'If needed'
+);
+
+$curlOptions = array(
+    //this will not work because the option is not correct. Need to change to the correct option
+    ANY_PHP_CURL_OPTION => 'Value'
+);
+
+SimpleCurl::Post(
+    'https://postman-echo.com/post',
+    $params,
+    $cookieFilePath,
+    $externalHeaders,
+    $curlOptions
+);
